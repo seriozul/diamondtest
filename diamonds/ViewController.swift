a902316393b64a0187853adfc5ce4785bd085195
lg <lg@lgs-Mac.local>
2016-07-07 05:28:43 -0700

    Added to git

--- diamonds/ViewController.swift
+++ diamonds/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueDiamond: UIImageView!
+    @IBOutlet weak var redDiamond: UIImageView!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    let d = "Diamond"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueDiamond(sender: AnyObject) {
+        if blueDiamond.hidden {
+            blueDiamond.hidden = false
+            blueButton.setTitle("Hide Blue \(d)", forState: UIControlState.Normal)
+        }
+        else {
+            blueDiamond.hidden = true
+            blueButton.setTitle("Show Blue \(d)", forState: UIControlState.Normal)
+        }
+    }
 
+    @IBAction func hideRedDiamond(sender: AnyObject) {
+        if redDiamond.hidden{
+            redDiamond.hidden = false
+            redButton.setTitle("Hide Red \(d)", forState: UIControlState.Normal)
+        }
+        else{
+            redDiamond.hidden = true
+            redButton.setTitle("Show Red \(d)", forState: UIControlState.Normal)
+        }
+    }
 }
 
